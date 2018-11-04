9898e981dfa0e3addaec11cddc2e60fcefa0dfd9
isoppp <prog@d2deck.com>
2018-11-04 22:23:45 +0900

    add authors type and resolvers

--- hackerbookreview_starter/server/src/resolvers.js
+++ hackerbookreview_starter/server/src/resolvers.js
@@ -1,11 +1,12 @@
 import { allBooks, imageUrl } from './book'
+import { authorsByBookId } from './authors'
 
 export default {
   Book: {
-    imageUrl: (book, { size }) => imageUrl(size, book.googleId)
+    imageUrl: (book, { size }) => imageUrl(size, book.googleId),
+    authors: book => authorsByBookId(book.id),
   },
   Query: {
-    // = type Query in typedefs.js
     books: () => allBooks(),
   },
 }
