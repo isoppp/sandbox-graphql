8c3319f857ba41b86cf17be81d840434cfc6a8c9
isoppp <prog@d2deck.com>
2018-11-14 07:49:46 +0900

    add real data in book detail page

--- hackerbookreview_starter/web/src/Book.js
+++ hackerbookreview_starter/web/src/Book.js
@@ -4,20 +4,56 @@ import { BookDetail } from './components/Book';
 import { BookDetailReviews } from './components/Book';
 import Error from './components/Error';
 import data from './data';
+import fetch from './fetch'
 
 const findBookById = (id, books) => R.find(R.propEq('id', id), books);
 
+const query = `
+fragment Book on Book {
+  id
+  title
+  description
+  imageUrl
+  rating
+}
+
+fragment Review on Review {
+  id
+  title
+  rating
+  comment
+  user {
+    name
+    imageUrl
+  }
+}
+
+query Book($id:ID!) {
+	book(id: $id) {
+    ...Book
+    reviews {
+      ...Review
+    }
+    authors {
+      name
+    }
+  }
+}
+`
 class Book extends Component {
   state = {
     book: null,
     errors: [],
   };
-  async componentDidMount() {
+
+  async componentDidMount () {
     const id = R.path(['props', 'match', 'params', 'id'], this);
     try {
-      // TODO: fetch actual book using graphql
-      const book = findBookById(id, data.books);
-      const errors = [];
+      const variables = { id }
+      const result = await fetch({ query, variables })
+      const book = R.path(['data', 'book'], result)
+      const errorList = R.pathOr([], ['errors'], result)
+      const errors = R.map(error => error.message, errorList)
       this.setState({ book, errors });
     } catch (err) {
       this.setState({ errors: [err.message] });
