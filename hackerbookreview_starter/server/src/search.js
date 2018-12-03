ae7c76b3375f66630b86818b62ee6ac2e5a1acfc
isoppp <prog@d2deck.com>
2018-12-03 13:49:09 +0900

    add other type resolvers

--- hackerbookreview_starter/server/src/search.js
+++ hackerbookreview_starter/server/src/search.js
@@ -3,7 +3,10 @@ import query from './db'
 
 export async function search (term) {
   const books = await searchBooks(term)
-  return [...books]
+  const users = await searchUser(term)
+  const authors = await searchAuthors(term)
+  const reviews = await searchReviews(term)
+  return [...books, ...users, ...authors, ...reviews]
 }
 
 async function searchBooks(term) {
@@ -16,6 +19,54 @@ async function searchBooks(term) {
     const params = [term]
     const result = await query(sql, params)
     return map(obj => ({ ...obj, __type: 'Book' }), result.rows)
+  } catch(err) {
+    console.log(err)
+    throw err
+  }
+}
+
+async function searchUser(term) {
+  const sql = `
+  select * from hb.user
+  where tokens @@ to_tsquery($1);
+  `
+
+  try {
+    const params = [term]
+    const result = await query(sql, params)
+    return map(obj => ({ ...obj, __type: 'User' }), result.rows)
+  } catch(err) {
+    console.log(err)
+    throw err
+  }
+}
+
+async function searchAuthors(term) {
+  const sql = `
+  select * from hb.author
+  where tokens @@ to_tsquery($1);
+  `
+
+  try {
+    const params = [term]
+    const result = await query(sql, params)
+    return map(obj => ({ ...obj, __type: 'Author' }), result.rows)
+  } catch(err) {
+    console.log(err)
+    throw err
+  }
+}
+
+async function searchReviews(term) {
+  const sql = `
+  select * from hb.review
+  where tokens @@ to_tsquery($1);
+  `
+
+  try {
+    const params = [term]
+    const result = await query(sql, params)
+    return map(obj => ({ ...obj, __type: 'Review' }), result.rows)
   } catch(err) {
     console.log(err)
     throw err
